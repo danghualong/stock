e3bafa156bc8914252c296fa8389fac4e0241920
danghualong <dhl_345@qq.com>
2021-03-18 23:00:26 +0800

    增加wfastcgi模块

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from src import createApp
+import os
 
 app = createApp()
+
+
+@app.route("/")
+def index():
+    mode=os.getenv("FLASK_ENV","ZZ")
+    return "Hello Flask {0}".format(mode)
+
 
 if __name__ == '__main__':
     app.run()
--- src/__init__.py
+++ src/__init__.py
@@ -7,7 +7,7 @@ from . import cron, logger, db
 def createApp(configName=None):
     app = Flask(__name__)
     if configName == None:
-        configName = os.getenv("FLASK_ENV", "development")
+        configName = os.getenv("FLASK_ENV", "production")
     app.config.from_object(configs[configName])
     logger.init_app(app)
     db.init_app(app)
