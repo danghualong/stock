fdad5c0f2efe81e8446bf4dd9bfe8e16d9e691d8
danghualong <dhl_345@qq.com>
2021-03-19 19:04:48 +0800

    增加json数据返回

--- src/model/api_model.py
+++ src/model/api_model.py
@@ -1,6 +1,6 @@
 from werkzeug.exceptions import HTTPException
 from werkzeug.wrappers.response import Response
-
+import json
 
 
 class JsonResult(object):
@@ -14,19 +14,33 @@ class JsonResult(object):
     def getHeaders(self):
         return [('Content-Type', 'application/json')]
     def getBody(self):
+        pass
 
-    
 
 class APIException(HTTPException):
     code = 200
     error_code = 999
     msg = "unknown error"
 
-    def __init__(self, msg, error_code):
+    def __init__(self,error_code=None, msg=None, code=None):
+        if (code):
+            self.code = code
         if (msg):
             self.msg = msg
         if (error_code):
             self.error_code = error_code
         super(HTTPException, self).__init__(self.msg)
     
+    def get_body(self, environ=None):
+        body = dict(
+            msg=self.msg,
+            error_code=self.error_code)
+        return json.dumps(body)
+    
+    def get_headers(self, environ=None):
+        """Get a list of headers."""
+        return [('Content-Type', 'application/json')]
+    
+class BadRequest(APIException):
+    pass
     
