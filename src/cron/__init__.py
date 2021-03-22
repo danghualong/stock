9138d0ec450c437a5c2a6236db1a46cb2d2490bc
danghualong <dhl_345@qq.com>
2021-03-22 18:36:51 +0800

    防止多进程造成定时任务触发多次

--- src/cron/__init__.py
+++ src/cron/__init__.py
@@ -5,8 +5,42 @@ from .service import add_stocks_service as ass_all
 from .service import add_history_service as ahs
 from .service import add_current_daily_service as acds
 from ..logger import currentLogger
+import platform,atexit
 
 def init_app(app):
+    if platform.system() != 'Windows':
+        fcntl = __import__("fcntl")
+        f = open('scheduler.lock', 'wb')
+        try:
+            fcntl.flock(f, fcntl.LOCK_EX | fcntl.LOCK_NB)
+            startJobs(app)
+        except:
+            pass
+
+        def unlock():
+            fcntl.flock(f, fcntl.LOCK_UN)
+            f.close()
+
+        atexit.register(unlock)
+    else:
+        msvcrt = __import__('msvcrt')
+        f = open('scheduler.lock', 'wb')
+        try:
+            msvcrt.locking(f.fileno(), msvcrt.LK_NBLCK, 1)
+            startJobs(app)
+        except:
+            pass
+
+        def _unlock_file():
+            try:
+                f.seek(0)
+                msvcrt.locking(f.fileno(), msvcrt.LK_UNLCK, 1)
+            except:
+                pass
+
+        atexit.register(_unlock_file)
+
+def startJobs(app):
     scheduler = APScheduler(BackgroundScheduler(timezone="Asia/Shanghai"))
     scheduler.init_app(app)
     scheduler.start()
